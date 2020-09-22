53017c29d5b079394a76bd9bcefb542a2957693c
Glitch (hackathonv4) <none>
2020-09-22 23:20:11 +0000

    🦄🎋 Added the pr and pat waiting room, the new index.html page, and a reviewpage.

--- prSignUp.js
+++ prSignUp.js
@@ -34,10 +34,21 @@ $('.tab a').on('click', function (e) {
   $(this).parent().addClass('active');
   $(this).parent().siblings().removeClass('active');
   
-  target = $(this).attr('href');
+  var target = $(this).attr('href');
 
   $('.tab-content > div').not(target).hide();
   
   $(target).fadeIn(600);
   
+});
+
+$('#signUp').on('submit',function() {
+jQuery(function ($) {
+    var $inputs = $('input[name=firstName],input[name=lastName], ,input[name=email], ,input[name=password],input[name=address],input[name=phoneNumber]');
+    $inputs.on('input', function () {
+        // This method only works on text types. You're going to need to look up validation for things that aren't text type.
+        var total = $('input[name=firstName]').val().length + $('input[name=lastName]').val().length + $('input[name=email]').val().length + $('input[name=password]').val().length+ $('input[name=address]').val().length + $('input[name=phoneNumber]').val().length;
+        $inputs.not(this).prop('required', !total);
+    });
+  });
 });
