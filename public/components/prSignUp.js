f5cb4919f9e430df20993891759ccdd0690f5877
Lillian Chou <lillianchou94@gmail.com>
2020-12-11 15:27:19 -0800

    Move all the files under /public (#34)

---
+++ public/components/prSignUp.js
@@ -0,0 +1,42 @@
+$(".form")
+  .find("input, textarea")
+  .on("keyup blur focus", function (e) {
+    var $this = $(this),
+      label = $this.prev("label");
+
+    if (e.type === "keyup") {
+      if ($this.val() === "") {
+        label.removeClass("active highlight");
+      } else {
+        label.addClass("active highlight");
+      }
+    } else if (e.type === "blur") {
+      if ($this.val() === "") {
+        label.removeClass("active highlight");
+      } else {
+        label.removeClass("highlight");
+      }
+    } else if (e.type === "focus") {
+      if ($this.val() === "") {
+        label.removeClass("highlight");
+      } else if ($this.val() !== "") {
+        label.addClass("highlight");
+      }
+    }
+  });
+
+// This is called when either "sign up" or "log in" tab is clicked.
+// It changes the "active" tab by adding 'active' to the clicked tab's class 
+// and removing 'active' from the other.
+$(".tab a").on("click", function (e) {
+  e.preventDefault();
+
+  $(this).parent().addClass("active");
+  $(this).parent().siblings().removeClass("active");
+
+  var target = $(this).attr("href");
+
+  $(".tab-content > div").not(target).hide();
+
+  $(target).fadeIn(600);
+});
