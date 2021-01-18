52753e687211531508d6e91adb96807188d65d9a
Steve <stevel@csedge.org>
2021-01-18 01:04:23 -0800

    Commented and Cleaned Up Code

--- public/components/demo.js
+++ public/components/demo.js
@@ -3,23 +3,17 @@ var video2Complete = false
 var video1 = document.getElementById('video1');
 var video2 = document.getElementById('video2');
 
-
+//Checks to see if the left-hand video has finished
 video1.addEventListener('ended', function () {
     video1Complete = true;
+    //Checks to see if both videos are finished before reseting both videos
     if (video2Complete == true) {
         video1.play();
         video2.play();
     }
   })
-
+//Checks to see if the right-hand video has finished
 video2.addEventListener('ended', function () {
+    //Signals that the video is completed
     video2Complete = true;
   })
-
-//   if (video1Complete == true && video2Complete == true) {
-//     document.querySelector('#video2').play();
-//     document.querySelector('#video1').play();
-//     console.count('loop restart');
-//     video1Complete = false;
-//     video2Complete = false
-//   }
