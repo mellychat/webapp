b753134b4d4dcea1201fd322e248765751e928c3
Steve <stevel@csedge.org>
2021-01-21 10:59:54 -0800

    Added canplaythrough event listener

--- public/components/demo.js
+++ public/components/demo.js
@@ -1,7 +1,24 @@
 var video1Complete = false;
-var video2Complete = false
+var video2Complete = false;
+var video1ReadyToStart = false;
+var video2ReadyToStart = false;
 var video1 = document.getElementById('video1');
 var video2 = document.getElementById('video2');
+
+video1.addEventListener('canplaythrough', (event) => {
+ video1ReadyToStart = true;
+      //Checks to see if both videos are ready to start before startning them
+      if (video2ReadyToStart == true) {
+        video1.play();
+        video2.play();
+    }
+});
+
+video2.addEventListener('canplaythrough', (event) => {
+  video2ReadyToStart = true;
+});
+
+
 
 //Checks to see if the left-hand video has finished
 video1.addEventListener('ended', function () {
@@ -17,3 +34,5 @@ video2.addEventListener('ended', function () {
     //Signals that the video is completed
     video2Complete = true;
   })
+
+
