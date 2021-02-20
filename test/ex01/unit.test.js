6eea483bb1bdc870405d121bef7748b6802c5255
Steve <stevel@csedge.org>
2021-02-20 01:05:11 +0000

    UPdated Solution in Unit Test

--- test/ex01/unit.test.js
+++ test/ex01/unit.test.js
@@ -22,13 +22,11 @@ import {
 } from "./constants.js";
 
 // TODO: Add a statement with your solutions
-import * as MY_SOLUTION from "./solution";
 import * as STEVE_SOLUTION from "./steve.solution";
 
 // TODO: add it to the list of test suites.
 const testSuites = [
-  { name: "My Solution", solution: MY_SOLUTION },
-  { name: "Template Solution", solution: STEVE_SOLUTION },
+  { name: "Steve's Solution", solution: STEVE_SOLUTION },
 ];
 
 /**
